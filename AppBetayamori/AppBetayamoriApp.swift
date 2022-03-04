550cbdec3ddb940bbe9f40b306eddd9f7563dee4
ryota-taka <ryota.t_31098is@icloud.com>
2022-03-04 18:02:17 +0900

    Initial Commit

---
+++ AppBetayamori/AppBetayamoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBetayamoriApp.swift
+//  AppBetayamori
+//
+//  Created by 高橋良汰 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct AppBetayamoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppBetayamori/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppBetayamori
+//
+//  Created by 高橋良汰 on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
